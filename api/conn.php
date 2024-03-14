39383950fa86aae9424ef1d37cdfc9d99a1d7722
saltarugio <vitalrugio@gmail.com>
2024-03-14 15:58:56 -0300

    update

--- api/conn.php
+++ api/conn.php
@@ -2,11 +2,7 @@
 $username = "root";
 $password = "root";
 $database = "letreco";
-<<<<<<< HEAD
 $hostname = "127.0.0.1";
-=======
-$hostname = "127.0.0.1"
->>>>>>> ad5b1d3d1e4baab985ded2948bfa3a0396808caf
 
 $conn = new mysqli($hostname, $username, $password, $database);
 if($conn->connect_errno){
