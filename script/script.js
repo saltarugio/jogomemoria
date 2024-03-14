8da59c1f04665ec2b41858f98a856bdbf97a8c00
saltarugio <vitalrugio@gmail.com>
2024-03-14 15:40:40 -0300

    update

--- script/script.js
+++ script/script.js
@@ -23,7 +23,7 @@ var verified = new Boolean(false);
 
 function loadword() {
     //const url = `http://localhost/hunterword/api/index.php`;
-    const url = `http://DESKTOP-J458M9S/hunterword/api/index.php`;
+    const url = `https://DESKTOP-J458M9S/hunterword/api/index.php`;
     fetch(url, {
         method: 'GET'
     }).then(response => {
